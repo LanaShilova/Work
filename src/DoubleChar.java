6c76a08993bc5fb408f22d065b9b30e5c4d460a6
sshilova <Svetlana.Shilova@t-systems.com>
2019-02-06 11:16:50 +0300

    Initial commit

---
+++ src/DoubleChar.java
@@ -0,0 +1,20 @@
+public class DoubleChar {
+
+    public String doubleChar(String str) {
+        String r = "";
+        for (int i = 0; i < str.length(); i++){
+            r+=str.charAt(i);
+            r+=str.charAt(i);
+        }
+        return r;
+    }
+    public static void main (String [] args) {
+        DoubleChar x = new DoubleChar();
+        DoubleChar y = new DoubleChar();
+        DoubleChar z = new DoubleChar();
+        System.out.println(x.doubleChar("The"));
+        System.out.println(y.doubleChar("AAbb"));
+        System.out.println(z.doubleChar("Hi-There"));
+
+
+    }}
