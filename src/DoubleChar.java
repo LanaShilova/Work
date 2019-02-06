43594f358a65f374499dbd391e5ffacf0c25ebb9
LanaShilova <46160638+LanaShilova@users.noreply.github.com>
2019-02-06 15:21:38 +0300

    --

--- src/DoubleChar.java
+++ src/DoubleChar.java
@@ -16,5 +16,5 @@ public class DoubleChar {
         System.out.println(y.doubleChar("AAbb"));
         System.out.println(z.doubleChar("Hi-There"));
 
-        System.out.println("I am tying to find out how Git works");
+ 
     }}
